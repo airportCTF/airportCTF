454fe79abeea7908bd2c46db29d07835a53ee9da
Danich <steam3335@list.ru>
2024-03-22 21:57:41 +0300

    My message

--- ticket/internal/ticket/ticket.go
+++ ticket/internal/ticket/ticket.go
@@ -1,9 +1,21 @@
 package ticket
 
-//func CreateTicket() *ticket.Ticket {
-//	return ticket.Ticket{}
-//}
-//
-//func rangeIn(low, hi int) int {
-//	return low + rand.Intn(hi-low)
-//}
+import (
+	"github.com/chessnok/airportCTF/core/pkg/flight"
+	"github.com/chessnok/airportCTF/core/pkg/ticket"
+	"math/rand"
+	"time"
+)
+
+func CreateTicket() *ticket.Ticket {
+	return &ticket.Ticket{
+		PNR:            0,
+		PassportNumber: 0,
+		Flight:         flight.Flight{},
+		Datetime:       time.Time{},
+	}
+}
+
+func rangeIn(low, hi int) int {
+	return low + rand.Intn(hi-low)
+}
